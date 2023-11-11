cd06966e4ffc27f6357a1ce40b14f2c80fcc99b2
williams-bruce <williamsbruceprep@gmail.com>
2023-11-11 17:55:18 -0300

    implementing votes

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from src.models.ConnectDB import ConnectDB
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '290665ddf7d91ef58dc2f6c500a15eb0'
+
+db = ConnectDB()
 
 from src import routes
--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,26 @@
 from flask import render_template
 from flask import request
 from src import app
-from src.forms import FormEleitor
+from src import db
 
 @app.route('/', methods = ['POST','GET'])
 def home():
     if request.method == 'POST':
+        print(request.form)
         numero = request.form.get('numero')
-        return f'<h1>O numero do eleitor é {numero}</h1>'
+        resultado = db.search('eleitores', codigo = numero)
+        if resultado:
+            return f'<h1>O numero do eleitor é {numero}</h1><br> e o nome é {resultado[0][0]}'
+        return f'<h1>O numero do eleitor é {numero}</h1><br>Mas esse eleitor não estã no BD'
     return render_template('urna-copy.html')
+
+
+@app.route('/vote', methods = ['POST', 'GET'])
+def vote():
+    if request.method == 'POST':
+        voto = request.form.get('voto')
+        resultado = db.search('candidatos', codigo = voto)
+        if resultado:
+            return # Mostrar foto, nome, partido, etc na mesma tela
+        
+    return render_template('vote.html')
