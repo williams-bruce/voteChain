cbcf8ceeee2635345569ef20e0cd645c97a355e8
williams-bruce <williamsbruceprep@gmail.com>
2023-11-11 17:05:13 -0300

    some tests

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,11 @@
 from flask import render_template
+from flask import request
 from src import app
+from src.forms import FormEleitor
 
-@app.route('/')
+@app.route('/', methods = ['POST','GET'])
 def home():
-    return render_template('urna.html')
+    if request.method == 'POST':
+        numero = request.form.get('numero')
+        return f'<h1>O numero do eleitor é {numero}</h1>'
+    return render_template('urna-copy.html')
