408e95c617a0234b6b42a451a16be10f459846a6
HamiltonMussi <hamiltonmussi.cf@gmail.com>
2023-11-12 14:55:48 -0300

    new pages

--- src/routes.py
+++ src/routes.py
@@ -3,7 +3,7 @@ from flask import request
 from src import app
 from src import db
 
-@app.route('/', methods = ['POST','GET'])
+"""@app.route('/', methods = ['POST','GET'])
 def home():
     if request.method == 'POST':
         print(request.form)
@@ -12,7 +12,18 @@ def home():
         if resultado:
             return f'<h1>O numero do eleitor é {numero}</h1><br> e o nome é {resultado[0][0]}'
         return f'<h1>O numero do eleitor é {numero}</h1><br>Mas esse eleitor não estã no BD'
-    return render_template('urna-copy.html')
+    return render_template('urna-copy.html')"""
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        numero = request.form.get('numero')
+        resultado = db.search('eleitores', codigo=numero)
+        if resultado:
+            return render_template('urna-copy.html', encontrado=True, nome=resultado[0][0])
+        return render_template('urna-copy.html', encontrado=False)
+    return render_template('urna-copy.html', encontrado=None)
+
 
 
 @app.route('/vote', methods = ['POST', 'GET'])
