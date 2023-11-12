8f0719f7cf3e57abcb71bb70cc71e2f4be3c6889
williams-bruce <williamsbruceprep@gmail.com>
2023-11-12 16:23:27 -0300

    changes

--- src/routes.py
+++ src/routes.py
@@ -6,32 +6,17 @@ from src import db
 @app.route('/', methods = ['POST','GET'])
 def home():
     vars = {
-        'page': 'inicio'
+        'page': 'inicio',
+        'eleitorFound': True
     }
     if request.method == 'POST':
-        
         numero = request.form.get('numero')
         resultado = db.search('eleitores', codigo = numero)
         if resultado:
-            vars['page'] = 'votacao'
-            return render_template('urna-copy-copy.html', **vars)
+            return render_template('urna-findstudent.html', **vars)
         else:
-            vars['page'] = 'eleitorNotFound'
-            return render_template('urna-copy-copy.html', **vars)
-    return render_template('urna-copy-copy.html', **vars)
+            vars['eleitorFound'] = False
+            return render_template('urna-findstudent.html', **vars)
+    return render_template('urna-initialpage.html', **vars)
 
 
-@app.route('/vote', methods = ['POST', 'GET'])
-def vote():
-    if request.method == 'POST':
-        voto = request.form.get('voto')
-        resultado = db.search('candidatos', codigo = voto)
-        if resultado:
-            return # Mostrar foto, nome, partido, etc na mesma tela
-        
-    return render_template('vote.html')
-
-
-@app.route('/confirmVote', methods = ['POST', 'GET'])
-def confirm_vote():
-    return render_template('confirm_vote.html')
